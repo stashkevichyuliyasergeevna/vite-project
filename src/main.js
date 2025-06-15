40659ce3d4e7d42ba9c52d2c7370785218f3f06b
Yuliya Sergeevna Stashkevich <stashkevichyuliyasergeevna@gmail.com>
2025-06-15 21:18:12 +0700

    asides

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
-import "./scss/style.scss";
+import './scss/style.scss'
 const elements = {
-  burgerButton: document.querySelector(".round-button--burger-button"),
-  burgerClose: document.getElementById("burgerClose"),
-  burgerMenu: document.querySelector(".burger-menu"),
-  blurOverlay: document.querySelector(".blur-overlay"),
-  
-};
-elements.burgerButton.addEventListener("click", () => {
-    elements.burgerMenu.style.display = "block";
-    elements.blurOverlay.style.display = "block";
-});
-elements.burgerClose.addEventListener("click", () => {
-    elements.burgerMenu.style.display = "none";
-    elements.blurOverlay.style.display = "none";
-});
+  burgerButton: document.querySelector('.round-button--burger-button'),
+  burgerClose: document.getElementById('burgerClose'),
+  burgerMenu: document.querySelector('.burger-menu'),
+  blurOverlay: document.querySelector('.blur-overlay')
+}
+elements.burgerButton.addEventListener('click', () => {
+  elements.burgerMenu.style.display = 'block'
+  elements.blurOverlay.style.display = 'block'
+})
+elements.burgerClose.addEventListener('click', () => {
+  elements.burgerMenu.style.display = 'none'
+  elements.blurOverlay.style.display = 'none'
+})
